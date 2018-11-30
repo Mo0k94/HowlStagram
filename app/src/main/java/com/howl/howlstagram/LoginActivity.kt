50658780fc7db1b562c77b13459591ffc889541b
user <tkdanr2427@naver.com>
2018-12-01 03:02:10 +0900

    로그인 자바 구현

--- app/src/main/java/com/howl/howlstagram/LoginActivity.kt
+++ app/src/main/java/com/howl/howlstagram/LoginActivity.kt
@@ -1,12 +1,58 @@
 package com.howl.howlstagram
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+        email_login_button.setOnClickListener {
+            createAndLoginEmail()
+        }
+
+    }
+
+    fun createAndLoginEmail(){
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())?.addOnCompleteListener {
+            task ->
+            if(task.isSuccessful){
+                moveMainPage(auth?.currentUser)
+                Toast.makeText(this,"아이디 생성이 완료되었습니다",Toast.LENGTH_LONG).show()
+            }else if(task.exception?.message.isNullOrEmpty()){
+                Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+            }else{
+                signinEmail()
+            }
+
+
+        }
+    }
+    fun signinEmail(){
+        auth?.signInWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())?.addOnCompleteListener {
+            task ->
+            if(task.isSuccessful){
+                moveMainPage(auth?.currentUser)
+                Toast.makeText(this,"로그인이 성공했습니다",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun moveMainPage(user : FirebaseUser?){
+
+        if(user != null){
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
